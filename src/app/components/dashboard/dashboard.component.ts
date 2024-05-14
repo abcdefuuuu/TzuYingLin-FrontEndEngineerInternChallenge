f085c5c2f5e903e935bd5293d4cc2095e9e8c140
zowie <C109118211@nkust.edu.tw>
2024-05-14 19:14:49 +0800

    feat: dashboard查詢

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ProductService} from "../../service/product.service";
 import {Product} from "../../share/models/product";
 
@@ -8,12 +8,28 @@ import {Product} from "../../share/models/product";
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  @ViewChild('dv') dataView: DataView | undefined;
   product!: Product[];
   constructor(private productService: ProductService) {}
   ngOnInit() {
     this.productService.getJsonData().subscribe(data => {
       this.product = data;
       console.log(data)
+    });
+  }
+  applyGlobalFilter(event: any) {
+    const keyword = event.target.value.toLowerCase();
+    if (keyword === '') {
+      this.loadInitialProductList();
+    } else {
+      this.product = this.product.filter(item =>
+        (item.name && item.name.toLowerCase().includes(keyword))
+      );
+    }
+  }
+  loadInitialProductList() {
+    this.productService.getJsonData().subscribe(data => {
+      this.product = data;
     });
   }
 }
